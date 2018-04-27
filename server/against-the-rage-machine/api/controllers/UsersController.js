79adaeacbc800f8d32d0310b396a392570ec0a85
Joel Brown <joel.brown@razorfish.com>
2018-04-27 15:38:51 -0400

    ATRM - node server with image endpoint

---
+++ server/against-the-rage-machine/api/controllers/UsersController.js
@@ -0,0 +1,12 @@
+/**
+ * UsersController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+  
+
+};
+
